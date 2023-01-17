519ccd4e88fa873381ae767ffd8184bfff34021a
no-yan <63000297+no-yan@users.noreply.github.com>
2023-01-18 04:03:54 +0900

    install brew if not installed

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
+
+set -e # -e: exit on error
+
+if [ ! "$(command -v brew)" ]; then
+  echo "Installing brew"
+  if [ "$(command -v curl)" ]; then
+    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+  elif [ "$(command -v wget)" ]; then
+    wget -qO- https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+  else
+    echo "To install chezmoi, you must have curl or wget installed." >&2
+    exit 1
+  fi
+else
+  chezmoi=chezmoi
+fi
+
 # Seemingly EOF is not needed, but I'm not sure about handling errors in shell.
 # https://www.chezmoi.io/user-guide/machines/macos/
 brew bundle --no-lock --file=/dev/stdin <<EOF
