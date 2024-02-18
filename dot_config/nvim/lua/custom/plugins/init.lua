3469a8faa8964f6a0f497c8ef13840b1d416366c
no-yan <63000297+no-yan@users.noreply.github.com>
2024-02-18 16:00:43 +0900

    auto change ime

--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+    {
+    -- インサートモードを抜ける時にIMEをOFFにする
+    "keaising/im-select.nvim",
+    config = function()
+        require("im_select").setup({})
+    end,
+    }
+}
