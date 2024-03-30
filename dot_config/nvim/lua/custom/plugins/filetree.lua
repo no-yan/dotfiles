83e8de2b71a02d89af2e8ee327ffd20d9da4a2e1
no-yan <63000297+no-yan@users.noreply.github.com>
2024-03-31 01:04:26 +0900

    <space>- でファイルツリーを開閉する設定を追加
    reveal_force_cwdはオフにしたい。cwdがファイルを開いているかで変わるため、暗黙的挙動が増え、適切なディレトリに移動するためにツリーを開閉することになる。
    reveal_force_cwdはcwdの外にあるファイルを開いた際に、自動的にcwdを開いているファイルに移動する。この設定はツリーが開いている時のみ動作する。

--- dot_config/nvim/lua/custom/plugins/filetree.lua
+++ dot_config/nvim/lua/custom/plugins/filetree.lua
@@ -1,5 +1,3 @@
--- File: lua/custom/plugins/filetree.lua
-
 return {
     "nvim-neo-tree/neo-tree.nvim",
     version = "*",
@@ -10,5 +8,28 @@ return {
     },
     config = function()
         require('neo-tree').setup {}
+        vim.keymap.set('n', '<leader>-', function()
+                local reveal_file = vim.fn.expand('%:p')
+                if (reveal_file == '') then
+                    reveal_file = vim.fn.getcwd()
+                else
+                    local f = io.open(reveal_file, "r")
+                    if (f) then
+                        f.close(f)
+                    else
+                        reveal_file = vim.fn.getcwd()
+                    end
+                end
+                require('neo-tree.command').execute({
+                    action = "focus",          -- OPTIONAL, this is the default value
+                    source = "filesystem",     -- OPTIONAL, this is the default value
+                    position = "left",         -- OPTIONAL, this is the default value
+                    reveal_file = reveal_file, -- path to file or folder to reveal
+                    reveal_force_cwd = true,   -- change cwd without asking if needed
+                    toggle = true,
+                })
+            end,
+            { desc = "Open neo-tree at current file or working directory" }
+        );
     end,
 }
