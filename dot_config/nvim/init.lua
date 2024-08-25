f5d1a70b8c0a4e9f6db53ddba56d5ab6dae2fecd
no-yan <63000297+no-yan@users.noreply.github.com>
2024-08-25 21:23:14 +0900

    vim: add lsp keymaps

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -382,6 +382,8 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>sc', builtin.lsp_incoming_calls, { desc = '[S]earch [C]all' })
+      vim.keymap.set('n', '<leader>so', builtin.lsp_outgoing_calls, { desc = '[S]earch [O]utgoing Call' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
