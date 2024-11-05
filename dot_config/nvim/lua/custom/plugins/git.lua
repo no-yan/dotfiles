e2b94070dc98a53fa1a3b4f9561a8e9bceed4a74
no-yan <63000297+no-yan@users.noreply.github.com>
2024-11-06 01:55:13 +0900

    feat(nvim): try neogit

--- dot_config/nvim/lua/custom/plugins/git.lua
+++ dot_config/nvim/lua/custom/plugins/git.lua
@@ -3,4 +3,17 @@ return {
     'sindrets/diffview.nvim',
     opts = {},
   },
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+
+      -- Only one of these is needed.
+      'nvim-telescope/telescope.nvim', -- optional
+      'ibhagwan/fzf-lua', -- optional
+      'echasnovski/mini.pick', -- optional
+    },
+    config = true,
+  },
 }
