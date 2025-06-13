5d18d4ea217383aab44df20d317279b96f865f6a
no-yan <63000297+no-yan@users.noreply.github.com>
2025-06-13 23:16:47 +0900

    fix

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/env bash
 set -euo pipefail
-sh -c "$(curl -fsLS get.chezmoi.io)" init --apply no-yan
+sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply no-yan
 chezmoi init --apply no-yan --purge-binary
